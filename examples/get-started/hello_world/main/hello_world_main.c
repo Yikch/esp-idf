a71d2facb0e86c8dc18843eae3222c3b5805af6c
Yikang <99170695+Yikch@users.noreply.github.com>
2024-09-24 18:46:29 +0200

    Add files via upload

--- examples/get-started/hello_world/main/hello_world_main.c
+++ examples/get-started/hello_world/main/hello_world_main.c
@@ -11,7 +11,25 @@
 #include "freertos/task.h"
 #include "esp_chip_info.h"
 #include "esp_flash.h"
-#include "esp_system.h"
+
+void vATaskFunction( void *pvParameters )
+{
+    while(1){
+        printf("Llamada desde task A: reinicio en %d msegundos...\n", CONFIG_TASKA_PERIOD);
+        vTaskDelay(CONFIG_TASKA_PERIOD / portTICK_PERIOD_MS);
+        printf("Llamada desde task A: reiniciando...\n");
+    }
+}
+
+
+void vBTaskFunction( void *pvParameters )
+{
+    while(1){
+        printf("Llamada desde task B: reinicio en %d msegundos...\n", CONFIG_TASKB_PERIOD);
+        vTaskDelay(CONFIG_TASKB_PERIOD / portTICK_PERIOD_MS);
+        printf("Llamada desde task B: reiniciando...\n");
+    }
+}
 
 void app_main(void)
 {
@@ -21,12 +39,11 @@ void app_main(void)
     esp_chip_info_t chip_info;
     uint32_t flash_size;
     esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+    printf("This is %s chip with %d CPU core(s), WiFi%s%s%s, ",
            CONFIG_IDF_TARGET,
            chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "",
            (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
 
     unsigned major_rev = chip_info.revision / 100;
@@ -42,11 +59,18 @@ void app_main(void)
 
     printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    printf("Chip has 2.4GHz WiFi: %s\n", CHIP_FEATURE_WIFI_BGN ? "true" : "false");
+
+    xTaskCreate( vATaskFunction, "taskA", 2048, NULL, 5, NULL );
+
+    xTaskCreate( vBTaskFunction, "taskB", 2048, NULL, 5, NULL );
+
+
+    // for (int i = 10; i > 0; i-=2) {
+    //     printf("Restarting in %d seconds...\n", i);
+    //     vTaskDelay(2000 / portTICK_PERIOD_MS);
+    // }
+    // printf("Restarting now.\n");
+    // fflush(stdout);
+    // esp_restart();
 }
